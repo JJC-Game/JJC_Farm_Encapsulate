6a693b2aa07bdcda3896623cdf4cbbe5cc5f23f6
sakagamisinobu <128011063+sakagamisinobu@users.noreply.github.com>
2023-06-30 11:55:51 +0900

    できました

---
+++ Assets/Scripts/Harvest/BaseHarvestEffect_202503_09.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHarvestEffect_202503_09 : BaseHarvestEffect
+{
+    override public void Initialize()
+    {
+
+    }
+
+    override public void Update()
+    {
+
+    }
+
+    override public void PlayHarvest()
+    {
+
+    }
+
+}
