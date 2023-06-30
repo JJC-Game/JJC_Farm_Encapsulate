158f5b53e19195de3b856f41a19c313ed0b0bde7
kosei <128009138+vally7469@users.noreply.github.com>
2023-06-30 12:24:31 +0900

    プログラムとエフェクト

--- Assets/Scripts/Harvest/Effect202503_13.cs
+++ Assets/Scripts/Harvest/Effect202503_13.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Effect202503_13 : BaseHarvestEffect
 {
+    private GameObject yamaguchi_effectPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        yamaguchi_effectPrefab = Resources.Load<GameObject>("Prefabs/HarvestEffect/202503_13Yamaguchi/FarmEffect");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
