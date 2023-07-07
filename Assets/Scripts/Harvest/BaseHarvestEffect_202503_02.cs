4ebf4695be3e6f135ac3b8a74b3357dedf579d54
ikeda <kuataic@gmail.com>
2023-07-07 11:05:16 +0900

    スクリプト編集

--- Assets/Scripts/Harvest/BaseHarvestEffect_202503_02.cs
+++ Assets/Scripts/Harvest/BaseHarvestEffect_202503_02.cs
@@ -4,21 +4,41 @@ using UnityEngine;
 
 public class BaseHarvestEffect_202503_02 : BaseHarvestEffect
 {
-    
+    private float ikeda_timer;
+    private bool ikeda_isPlaying;
+    private GameObject ikeda_effectPrefab;
+    private GameObject ikeda_effectInstance;
     // Start is called before the first frame update
     public override void Initialize()
     {
-        
+        ikeda_timer = 0.0f;
+        ikeda_isPlaying = false;
+        ikeda_effectPrefab = Resources.Load<GameObject>("Prefabs/HarvestEffect/202503_02/IkedaEffect");
+        ikeda_effectInstance = null;
     }
 
     // Update is called once per frame
     public override  void Update()
     {
-        
+        if (ikeda_isPlaying)
+        {
+            ikeda_timer -= Time.deltaTime;
+            if (ikeda_timer < 0.0f)
+            {
+                GameObject.Destroy(ikeda_effectInstance);
+                ikeda_isPlaying = false;
+                ikeda_effectInstance = null;
+            }
+        }
     }
 
     public override void PlayHarvest()
     {
-
+        if (ikeda_effectInstance == null)
+        {
+            ikeda_effectInstance = GameObject.Instantiate(ikeda_effectPrefab);
+            ikeda_timer = 2.0f;
+            ikeda_isPlaying = true;
+        }
     }
 }
